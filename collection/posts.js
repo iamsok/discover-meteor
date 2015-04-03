4daba5a7d2e9be49c66f7640c64bd4eec70b582a
David Sok <sokd88@gmail.com>
2015-04-03 12:55:01 -0400

    Added data to the posts collection

--- collection/posts.js
+++ collection/posts.js
@@ -1,1 +1,14 @@
-Posts = new Meteor.Collection('posts');
+Posts = new Mongo.Collection('posts');
+
+PostsSchema = new SimpleSchema({
+  title: {
+    type: String,
+    max: 100
+  },
+  url: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Url
+  }
+});
+
+Posts.attachSchema(PostsSchema);
